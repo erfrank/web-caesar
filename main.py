057f39d1eb725321d1a93cf0a3e7795c8492e165
Erica Franklin <Erica@Ericas-MacBook-Air.local>
2018-10-25 19:30:45 -0500

    hopefully dne iwth web caesar

--- main.py
+++ main.py
@@ -5,47 +5,49 @@ app = Flask(__name__)
 app.config['Debug']=True
 
 form = """
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-           }}
-        </style>
-    </head>
-    <body>
-     <form>
-        <label> Rotate by: 
-        <input type="text" name="rot" value=0/> 
-        </label> <br/>
-        <textarea name="text">{0}</textarea> 
-        <br/>
-        <button type="submit" value="submit">Submit Query</button>
-    </form>
-    </body>
-</html>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                form {{
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }}
+                textarea {{
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+            }}
+            </style>
+        </head>
+        <body>
+        <form action="/encrypt" method="POST">
+            <label> Rotate by: 
+            <input type="text" name="rot" value="0" /> 
+            </label> <br/>
+            <textarea name="text">{0}</textarea> 
+            <br/>
+            <button type="submit" value="submit">Submit Query</button>
+        </form>
+        </body>
+    </html>
 """
-@app.route("/")
+
+@app.route("/", methods = ['GET'])
 def index():
      return form.format("")
 
-@app.route("/", methods=['POST'])
+@app.route("/encrypt", methods=['POST'])
 def encrypt():
     text = request.form['text']
     rot =int(request.form['rot'])
     answer = rotate_string(text,rot)
     return form.format(answer)
-
+  
 app.run()
 
 
